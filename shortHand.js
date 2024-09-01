efa0b7f2692774105562b83ddf46c42a65a5dc9c
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-01 21:29:09 +0800

    Lesson 3

---
+++ shortHand.js
@@ -0,0 +1,22 @@
+var price = 10;
+
+price += 20;
+console.log(price);
+
+var laptopPrice = 50000;
+
+laptopPrice -= 20000;
+
+console.log(laptopPrice);
+
+var potatoPrice = 60
+
+potatoPrice *= 2;
+
+console.log(potatoPrice);
+
+var candyPrice = 5;
+
+candyPrice /= 2;
+
+console.log(candyPrice);
