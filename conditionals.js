98818f99c137aafcaea1908f59e6cf02c7401d54
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-02 21:04:07 +0800

    js conditions

---
+++ conditionals.js
@@ -0,0 +1,54 @@
+// console.log(5 > 10)
+// console.log(5 < 10)
+
+// const onionPrice = 120;
+// const eggPrice = 10;
+
+// Check if onion price is more than egg price
+
+// if (onionPrice > eggPrice){
+//   console.log("Onion is more expensive than egg.");
+// }
+// else {
+//   console.log("Egg is more expensive than onion.");
+// }
+
+// ----------------------------------------------------------------
+
+// const age = 12;
+// if (age >= 10) {
+//   console.log("You are allowed to enter the park.");
+// } else {
+//   console.log("You are not allowed to enter the park.");
+// }
+
+// ----------------------------------------------------------------
+
+// const age = 25;
+
+// if (age >= 18) {
+//   console.log("You are allowed to go to the Ride.");
+// } else if (age >= 13) {
+//   console.log("You are allowed to got few Rides only.");
+// } else {
+//   console.log("You are not allowed to go to the Ride.");
+// }
+
+// ----------------------------------------------------------------
+
+let age = 20;
+let withAdult = true;
+
+if (age >= 15) {
+  console.log("You are allowed to enter the park");
+  if (withAdult) {
+    console.log("You can go tho the haunted house at the park");
+  } else {
+    console.log("You cannot go tho the haunted house at the park");
+  }
+} else {
+  console.log("You are not allowed to enter the park");
+}
+
+// ----------------------------------------------------------------
+
