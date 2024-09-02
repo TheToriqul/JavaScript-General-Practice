4a669810c752f623c2609f5b0367602605236c35
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-02 21:12:49 +0800

    Multiple conditions

--- conditionals.js
+++ conditionals.js
@@ -1,10 +1,11 @@
+// ----------------------------  Just conditions ------------------------------------
 // console.log(5 > 10)
 // console.log(5 < 10)
 
 // const onionPrice = 120;
 // const eggPrice = 10;
 
-// Check if onion price is more than egg price
+// ------------------------if else conditions------------------------
 
 // if (onionPrice > eggPrice){
 //   console.log("Onion is more expensive than egg.");
@@ -14,7 +15,6 @@
 // }
 
 // ----------------------------------------------------------------
-
 // const age = 12;
 // if (age >= 10) {
 //   console.log("You are allowed to enter the park.");
@@ -34,14 +34,14 @@
 //   console.log("You are not allowed to go to the Ride.");
 // }
 
-// ----------------------------------------------------------------
+// -----------------------------Multiple conditions-----------------------------------
 
 let age = 20;
-let withAdult = true;
+let withAdult = false;
 
-if (age >= 15) {
+if (age >= 12 || withAdult) {
   console.log("You are allowed to enter the park");
-  if (withAdult) {
+  if (age >= 20 && withAdult) {
     console.log("You can go tho the haunted house at the park");
   } else {
     console.log("You cannot go tho the haunted house at the park");
