5596d8f23f8bf441fe295a2f9a0f036532ce0c62
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-03 21:59:59 +0800

    property.js

---
+++ property.js
@@ -0,0 +1,10 @@
+const car = {
+  model : "BMW",
+  color : "Red",
+  engine: "4 HP",
+  price: 6500000,
+  isFavorite: true  
+}
+console.log(car)
+console.log(car.color)
+console.log(car["price"])
