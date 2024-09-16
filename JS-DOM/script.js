158ac5c3bcdc78e0f02994afd4db36803ef48db8
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-16 09:11:25 +0800

    DOM practices

--- JS-DOM/script.js
+++ JS-DOM/script.js
@@ -3,11 +3,20 @@ button.addEventListener("click", () => {
   console.log("Button clicked!");
 });
 
+// Option : 1
 const makeGreen = document.getElementById('make-green');
 makeGreen.addEventListener("click", function (){
   document.body.style.backgroundColor = 'green';
 });
 
+// Option : 2
+
 function makeRed(){
   document.body.style.backgroundColor = 'red';
 }
+
+// Option : 3
+const userEmail = document.getElementById('user-email');
+userEmail.addEventListener("keyup", function(event){
+  console.log(event.target.value);
+})
