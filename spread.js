ecfe579fbcec504df3b44bdfe35be08a5dd8c16e
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-29 00:48:04 +0800

    spread

--- spread.js
+++ spread.js
@@ -1,2 +1,4 @@
 const max = Math.max(6, 23, 45, 1, 89, 23, 112, 125, 54, 12, 48, 14, 48);
-console.log(max);
+const numbers = [6, 23, 45, 1, 89, 23, 112, 125, 54, 12, 48, 14, 48];
+const arrayMax = Math.max(...numbers);
+console.log(arrayMax);
